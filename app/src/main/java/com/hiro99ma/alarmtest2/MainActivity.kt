7aea025ed07fd53d4774a526f3328984ff4994a7
hirokuma <hiro9ma@gmail.com>
2022-08-21 14:48:34 +0900

    add Notification
    AlarmでBroadcastReceiverからNotification表示。
    Notificationからアプリ起動。

--- app/src/main/java/com/hiro99ma/alarmtest2/MainActivity.kt
+++ app/src/main/java/com/hiro99ma/alarmtest2/MainActivity.kt
@@ -1,22 +1,37 @@
 package com.hiro99ma.alarmtest2
 
 import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private var alarmStarted = false
-    private val alarmInterval: Long = 15 * 60 * 1000 // 15min
+    private val alarmInterval: Long = 3 * 60 * 1000 // 3min
     private var pendingIntent: PendingIntent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        val name = "のてぃふぃけーしょんちゃんねる"
+        val descriptionText = "のてぃふぃけーしょんちゃんねるのですくりぷしょん"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(NOTIFICATION_ID, name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 
     override fun onResume() {
@@ -52,5 +67,8 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private val TAG = MainActivity::class.java.name
+
+        const val NOTIFICATION_ID = "10000"
+        const val NOTIFY_TEST_ID = 10000
     }
 }
--- app/src/main/java/com/hiro99ma/alarmtest2/NotificationReceiver.kt
+++ app/src/main/java/com/hiro99ma/alarmtest2/NotificationReceiver.kt
@@ -1,13 +1,42 @@
 package com.hiro99ma.alarmtest2
 
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.core.app.NotificationCompat
 
 class NotificationReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         Log.d(TAG, "Receive !!")
+        if (context == null) {
+            Log.d(TAG, "null context")
+            return
+        }
+
+        val builder = createNotification(context)
+        val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(MainActivity.NOTIFY_TEST_ID, builder.build())
+    }
+
+    private fun createNotification(context: Context): NotificationCompat.Builder {
+        // 自アプリを起こす
+        val wakeupMeIntent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val wakeupMePendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, wakeupMeIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        return NotificationCompat.Builder(context, MainActivity.NOTIFICATION_ID)
+            .setSmallIcon(com.google.android.material.R.drawable.ic_clock_black_24dp)
+            .setContentTitle("のてぃふぃけーしょんたいとる")
+            .setContentText("のてぃふぃけーしょんてきすと")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(wakeupMePendingIntent)
+            .setAutoCancel(true)
     }
 
     companion object {
